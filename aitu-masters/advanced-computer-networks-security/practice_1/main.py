53492b9475f3221eb70790104adf915e2b816432
NoyanTM <doge.channel@gmail.com>
2025-09-12 17:05:47 +0500

    2025-09-12_3

--- aitu-masters/advanced-computer-networks-security/practice_1/main.py
+++ aitu-masters/advanced-computer-networks-security/practice_1/main.py
@@ -4,13 +4,12 @@ import matplotlib.pyplot as plt
 from pyvis.network import Network
 
 def main():
-    packets = pyshark.FileCapture("./local_data/wirehark-capture_2025-09-10.pcapng")
+    packets = pyshark.FileCapture("./local_data/wireshark-capture_practice-1.pcapng")
 
     unique_nodes = set()
     edges = list()
 
     for packet in packets:
-        # print(dir(packet["ip"])
         source = ""
         distanation = ""
         try:
@@ -24,7 +23,6 @@ def main():
         unique_nodes.add(distanation)
         unique_nodes.add(source)
         edges.append((source, distanation))
-    # print(unique_nodes)
     
     graph = nx.DiGraph()
     graph.add_nodes_from(list(unique_nodes))
@@ -32,8 +30,6 @@ def main():
     network = Network("1000px", "1000px")
     network.from_nx(graph)
     network.write_html("./local_data/graph.html")
-    # nx.draw(graph, with_labels=True)
-    # plt.show()
 
 if __name__ == "__main__":
     main()
